bd86c848ea9383848216771ddfb7b69d8a67eb7e
Khamba <saurabhrsikchi@gmail.com>
2017-09-11 14:03:32 +0000

    changes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170826063650) do
+ActiveRecord::Schema.define(version: 20170904032751) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -76,6 +76,16 @@ ActiveRecord::Schema.define(version: 20170826063650) do
     t.integer  "order_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "provider"
+    t.string   "mobile_number"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+    t.string   "uid"
   end
 
   add_foreign_key "line_items", "products"
