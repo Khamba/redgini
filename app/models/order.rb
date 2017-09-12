dcd6d32d86e321426860d2a9b59378678666a4c8
Khamba <saurabhrsikchi@gmail.com>
2017-09-12 10:43:26 +0000

    changes

--- app/models/order.rb
+++ app/models/order.rb
@@ -16,4 +16,32 @@ class Order < ActiveRecord::Base
     has_one :shopping_cart
     belongs_to :user
     has_one :address, through: :user
+    
+    after_create :send_emails_and_texts
+    
+    private
+    
+        def send_emails_and_sms
+           send_email_to_customer
+           send_email_to_admin
+           send_text_to_customer
+           send_text_to_admin
+        end
+        
+        def send_email_to_customer
+           #TODO 
+        end
+        
+        def send_email_to_admin
+            #TODO
+        end
+        
+        def send_text_to_admin
+            #TODO
+        end
+        
+        def send_text_to_customer
+            #TODO
+        end
+    
 end
